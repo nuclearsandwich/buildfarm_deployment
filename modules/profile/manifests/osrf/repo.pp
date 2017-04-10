e28667329a540c0437f7f277e6188374d13c1cae
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-04-10 09:58:24 -0400

    Debugging.

--- modules/profile/manifests/osrf/repo.pp
+++ modules/profile/manifests/osrf/repo.pp
@@ -210,6 +210,7 @@ class profile::osrf::repo {
       require => User[$agent_username],
     }
 
+    notice('got here')
     # Pull reprepro updater
     notice(hiera('jenkins-agent::reprepro_config'))
     if hiera('jenkins-agent::reprepro_config', false){
