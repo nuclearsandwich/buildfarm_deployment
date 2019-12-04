3df2682409099a6c61c3f093f4ffcee8e70a9150
Steven! Ragnarök <steven@nuclearsandwich.com>
2019-12-04 11:37:56 -0500

    Use structs 1.20

--- modules/profile/manifests/jenkins/rosplugins.pp
+++ modules/profile/manifests/jenkins/rosplugins.pp
@@ -358,7 +358,7 @@ class profile::jenkins::rosplugins {
   }
 
   ::jenkins::plugin { 'structs':
-    version => '1.19',
+    version => '1.20',
     require => [ Jenkins::Plugin['command-launcher'], Jenkins::Plugin['jdk-tool'] ]
   }
 
